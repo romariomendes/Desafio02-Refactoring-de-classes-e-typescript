2cbf2c6609b383db7ffd1533f4e52c906f4cedab
Romario Mendes <romariomendesgoes@gmail.com>
2021-03-21 19:10:33 -0500

    Refactoring de classes e typescript complete

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,14 +3,19 @@ import {
   useRef,
   useState,
   useCallback,
-} from 'react';
+  HTMLAttributes,
+} from "react";
 
-import { useField } from '@unform/core';
+import { useField } from "@unform/core";
 
-import { Container } from './styles';
+import { Container } from "./styles";
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface InputProps extends HTMLAttributes<HTMLInputElement> {
+  name: string;
+}
+
+export const Input = ({ name, ...rest }: InputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -31,14 +36,12 @@ const Input = ({ name, icon: Icon, ...rest }) => {
     registerField({
       name: fieldName,
       ref: inputRef.current,
-      path: 'value',
+      path: "value",
     });
   }, [fieldName, registerField]);
 
   return (
     <Container isFilled={isFilled} isFocused={isFocused}>
-      {Icon && <Icon size={20} />}
-
       <input
         onFocus={handleInputFocus}
         onBlur={handleInputBlur}
@@ -49,5 +52,3 @@ const Input = ({ name, icon: Icon, ...rest }) => {
     </Container>
   );
 };
-
-export default Input;
